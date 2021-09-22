cb40aa577adb5a21eb4bcf4dbb16549b3991f4e4
Ulianchenko <ulianchenko@gmail.com>
2021-09-22 17:27:42 +0300

    add key in TodoListItem

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,8 +4,9 @@ import TodoListItem from "./todo-list-item"
 
 const TodoList = ({ todos }) => {
   const elements = todos.map((item) => {
+    const { id, ...itemProps } = item
     return (
-      <li key={item.id}><TodoListItem {...item}/></li>
+      <li key={id}><TodoListItem {...itemProps}/></li>
     );
   });
   return (
