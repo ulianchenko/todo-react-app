4ecb8468bd30f542861ef58dd58812464ebe0e63
Ulianchenko <ulianchenko@gmail.com>
2021-09-22 16:25:56 +0300

    main todo-react-app structure is created

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import AppHeader from "./components/app-header";
+import TodoList from "./components/todo-list";
+import SearchPanel from "./components/search-panel";
+
+const App = () => {
+
+  const todoData = [
+    { label: "Drink coffee", important: false },
+    { label: "Make awesome app", important: true },
+    { label: "Have a lunch", important: false }
+  ];
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={ todoData }/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
