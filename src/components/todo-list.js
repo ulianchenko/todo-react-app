3f0eb31174307e531bb53eb412f3eadfc806f787
Ulianchenko <ulianchenko@gmail.com>
2021-09-23 17:51:41 +0300

    add css in app

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-import TodoListItem from "./todo-list-item"
+import TodoListItem from "./todo-list-item";
+import "./todo-list.css";
 
 const TodoList = ({ todos }) => {
+
   const elements = todos.map((item) => {
-    const { id, ...itemProps } = item
+    const { id, ...itemProps } = item;
     return (
-      <li key={id}><TodoListItem {...itemProps}/></li>
+      <li key={id} className="list-group-item"><TodoListItem {...itemProps}/></li>
     );
   });
+  
   return (
-    <ul>
+    <ul className="list-group todo-list">
       { elements }
     </ul>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 import AppHeader from "./components/app-header";
 import TodoList from "./components/todo-list";
 import SearchPanel from "./components/search-panel";
+import ItemStatusFilter from "./components/item-status-filter";
+
+import "./index.css";
 
 const App = () => {
 
@@ -14,9 +17,13 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3}/>
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter/>
+      </div>
+
       <TodoList todos={ todoData }/>
     </div>
   );
