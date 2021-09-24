8af1293efd3bb5e9a9d33a2c506276dbf014e89c
Ulianchenko <ulianchenko@gmail.com>
2021-09-24 14:23:47 +0300

    put components inside directories, add classes instead of functions in some components

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./todo-list-item.css";
+
+export default class TodoListItem extends React.Component {
+
+
+  render() {
+
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal' 
+    };
+  
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>{ label }</span>
+  
+        <button type="button" className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+  
+        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+      </span>
+    );
+  };
+}
